5b7aab6c9dc04f7195fef7b36a43a89c02ab1ba1
ziauddin-0987 <ziauddink100@gmail.com>
2024-05-31 01:30:22 +0500

    this code  checking  a debugger

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function debugg(){
-    let arra = ["apple","banana","graps","apple"]
-    for
+function debugg() {
+  //   let pro = prompt("print pyour name");
+  let arra = ["apple", "banana", "apple", "graps", "apple", "banana", "kavi"];
+  let apples = [];
+  for (let i = 0; i < arra.length; i++) {
+    debugger;
+    if (arra[i] == "apple") {
+      apples.push(arra[i]);
+      console.log(`yas its frutes ${arra[i]} `);
+    }
+  }
 }
+debugg();
